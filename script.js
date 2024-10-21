a640db29e05567d6bcd6e1eaf632bdc45d7bd1bd
Rafhael1901 <140663087+Rafhael1901@users.noreply.github.com>
2024-10-21 10:54:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+
+    
+    const tabela = document.getElementById('tabela').getElementsByTagName('tbody')[0];
+    const novaLinha = tabela.insertRow();
+
+
+    const celulaNome = novaLinha.insertCell(0);
+    const celulaEmail = novaLinha.insertCell(1);
+
+    celulaNome.textContent = nome;
+    celulaEmail.textContent = email;
+
+
+    document.getElementById('form').reset();
+});
